6752922eafb7462af8b426af277fe9d33947f536
Jaycen <saitakusei@gmail.com>
2024-10-30 22:28:21 +0900

    Using params as pair & fix price bug

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
@@ -10,25 +10,39 @@ import forex.services.rates.errors._
 import sttp.client4.quick._
 import sttp.client4.Response
 
+import io.circe.parser
+
+
 class OneFrameClient[F[_]: Applicative] extends Algebra[F] {
 
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
-    println("OneFrameClient.get")
+    println("OneFrameClient.get pair " + pair)
+
     val response: Response[String] = quickRequest
-      .get(uri"http://localhost:8080/rates?pair=USDJPY")
+      .get(uri"http://localhost:8080/rates?pair=${pair.from.toString}${pair.to.toString}")
       .header("token", "10dc303535874aeccc86a8251e6992f5")
       .send()
 
-    println(response.code)
+    println("response.code " + response.code)
     // prints: 200
+    
+    val parsedResponse = parser.parse(response.body)
+    println(parsedResponse)
 
-    println(response.body)
-    // prints some JSON string
+    // Assuming the response is a JSON array with a single object
+    val price: Double = parsedResponse match {
+      case Right(json) =>
+        json.asArray.flatMap(_.headOption) match {
+          case Some(firstObject) =>
+            firstObject.hcursor.downField("price").as[Double] match {
+              case Right(value) => value
+              case Left(_) => 0.0
+            }
+          case None => 0.0
+        }
+      case Left(_) => 0.0
+    }
 
-    // Assuming you want to parse the response and return a Rate
-    // This part will depend on how you want to handle the response
-    // For now, let's assume a successful response returns a dummy Rate
-    Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F]
+    Rate(pair, Price(price), Timestamp.now).asRight[Error].pure[F]
   }
-
 }
